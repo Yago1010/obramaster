7997e6e5e0568aeb457ff99c33c4644e2996113c
Yago Ribeiro de Oliveira <yagoanjodopecado@gmail.com>
2026-01-24 17:32:50 -0300

    resolvendo concluto de imagens

--- src/lib/basePath.ts
+++ src/lib/basePath.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 export const basePath =   typeof window !== "undefined" &&
   window.location.pathname.startsWith("/JLConstrucao")
     ? "/JLConstrucao"
     : "";
-=======
-export const basePath =   process.env.NODE_ENV === "production"
-    ? "/JLConstrucao"
-    : "";
->>>>>>> f3ab5dfb9d7917b999d1cf1ae02d036420fc84b1
+
