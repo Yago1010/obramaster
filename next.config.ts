927b0cb65d307a7ba5cd826787ed1d60e2e6434e
Yago Ribeiro de Oliveira <88752094+Yago1010@users.noreply.github.com>
2026-01-24 16:56:04 -0300

    Simplify next.config by removing production paths
    Removed production-specific basePath and assetPrefix.

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
-
-  basePath: isProd ? "/JLConstrucao" : "",
-  assetPrefix: isProd ? "/JLConstrucao/" : "",
-
   images: {
     unoptimized: true,
   },
 };
 
 module.exports = nextConfig;
+
